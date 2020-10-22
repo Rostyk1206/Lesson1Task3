a09432f60c4d4abf61a0eecee553bf1742be0b9a
unknown <rostykvoznyak@gmail.com>
2020-10-22 20:43:54 +0300

    Lesson1Task3

---
+++ Lesson1Task3/Lesson1Task3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Lesson1Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal money= 10.54m;
+            Console.WriteLine(money);
+            
+        }
+    }
+}
